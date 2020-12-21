dc141e2c4013d9d6cb59786dc6343e9ca02d3e22
Dejan Alija <62359528+deyanaaliyah@users.noreply.github.com>
2020-12-21 10:00:10 +0100

    Add files via upload

---
+++ programming-2/src/main/java/kea/exam/demo/Programming2Application.java
@@ -0,0 +1,13 @@
+package kea.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Programming2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Programming2Application.class, args);
+    }
+
+}
---
+++ programming-2/src/test/java/kea/exam/demo/Programming2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Programming2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
